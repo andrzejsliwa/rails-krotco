1094259db6da083c8aa7fdada4baec6a9e58933d
andrzejsliwa <andrzej.sliwa@i-tool.eu>
2010-11-08 14:48:28 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KrotCo::Application.config.secret_token = 'd8b4c83037c5b69628de61046f4fddd014147ca38666d0dd5936fc85179e91a4850a7648e326f11afd0da07083f58f471aaf5556b79715682901097c5cf5f9d5'
